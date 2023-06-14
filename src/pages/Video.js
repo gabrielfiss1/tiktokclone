a408598e84e1d222cb26ce40b313f2ad842b6b89
Gabriel Fiss <gabriel--fiss@hotmail.com>
2023-06-14 20:32:17 -0300

    feat: video component

---
+++ src/pages/Video.js
@@ -0,0 +1,33 @@
+import React, {useRef, useState} from 'react'
+import "./video.css"
+function Video() {
+ 
+  const videoRef = useRef(null)
+  const [play, setPlay] = useState(false)
+
+  function handleStart(){
+
+    if(play === false){
+        videoRef.current.play()
+        setPlay(true)
+    } else {
+        videoRef.current.pause()
+        setPlay(false)
+    }
+  }
+
+  return (
+    <div className='video'>
+        <video
+         className='video_player'
+         ref={videoRef}
+         onClick={handleStart}
+         loop
+         src='https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z'
+         >
+        </video>
+    </div>
+  )
+}
+
+export default Video
